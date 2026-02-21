5d5189459e650b541bf2dd04f0773b8a7bdaa447
luckyassociation1107 <luckyassociation1107@gmail.com>
2026-02-21 22:53:06 +0530

    Refactor admin bot to use new command structure

--- admin_bot.py
+++ admin_bot.py
@@ -1,9 +1,22 @@
-from aiogram import Bot, Dispatcher, types
-from config import ADMIN_BOT_TOKEN
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+ADMIN_BOT_TOKEN = "8320896381:AAGJoHkAV-Dfz9u6lbGRFeyOc83ye-4vXgo"  # <-- admin token
 
 bot = Bot(token=ADMIN_BOT_TOKEN)
-admin_dp = Dispatcher()
+dp = Dispatcher()
 
-@admin_dp.message(commands=["start"])
-async def start(msg: types.Message):
-    await msg.answer("🔐 Admin Control Bot Active")
+
+@dp.message(Command("start"))
+async def admin_start(message: Message):
+    await message.answer("admin bot started 🔐")
+
+
+async def start_admin_bot():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_admin_bot())
